d33e4d13c3527d0293604c4af3bd8b41346547f8
tajaa <tajatheprince@gmail.com>
2024-02-04 17:58:32 -0800

    added ability to use form to insert information that we'll use with openai to get a request submitted for some output we want to represent on the index.html

--- app.py
+++ app.py
@@ -40,19 +40,24 @@ def get_colors(msg):
     return {"colors": colors}
 
 
+# @app.route("/")
+# def index():
+#    response = client.chat.completions.create(
+#        model="gpt-3.5-turbo-0125",
+#        messages=[
+#            {
+#                "role": "user",
+#                "content": "Name me a different neighborhood in San Francisco",
+#            }
+#        ],
+#    )
+# return response.choices[0].message.content
+# return render_template("index.html")
+
+
 @app.route("/")
 def index():
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
-        messages=[
-            {
-                "role": "user",
-                "content": "Name me a different neighborhood in San Francisco",
-            }
-        ],
-    )
-    return response.choices[0].message.content
-    # return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/palette", methods=["POST"])
